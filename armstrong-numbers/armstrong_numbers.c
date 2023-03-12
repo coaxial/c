59be5c0dd5b0a0e34744d7c862d9979720f650aa
coaxial <c@64b.it>
2023-03-12 14:48:34 +0100

    Improve armstrong-numbers solution

--- armstrong-numbers/armstrong_numbers.c
+++ armstrong-numbers/armstrong_numbers.c
@@ -1,26 +1,36 @@
 #include "armstrong_numbers.h"
-#include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
 
-bool is_armstrong_number(int candidate)
-{
-   if(candidate == 0)
-   {
-      return true;
-   }
-
-   int digits_count = floor(log10(abs(candidate))) + 1;
-   /* Initialize the calculation's result */
-   int armstrong_calc = 0;
-
+bool is_armstrong_number(int candidate) {
    // An Armstrong number is a number where the sum of each digit to the power
    // of the total number of digits is equal to the number itself.
    // i.e. 1^3 + 5^3 + 3^3 = 153 => 153 is an Armstrong number
    //      1^3 + 0^3 + 0^3 = 3   => 100 isn't one
-   for(int i = 0; i < digits_count; i++)
-   {
-      int digit = (int)(candidate / pow(10, i)) % 10;
+
+   if (candidate == 0) {
+      return true;
+   }
+
+   if (candidate < 0) {
+      return false;
+   }
+
+   int digits_count = log10(abs(candidate)) + 1;
+   int armstrong_calc = 0; // Tracks the calculation result.
+
+   // Copy candidate to avoid mutating it and failing the final comparison.
+   int leftover_digits = 0;
+   if (!memcpy(&leftover_digits, &candidate, sizeof(candidate))) {
+      return -1;
+   }
+
+   for(int i = 0; i < digits_count; i++) {
+      // Get the rightmost digit.
+      int digit = leftover_digits % 10;
+      // Remove the digit from the remaining digits.
+      leftover_digits /= 10;
       armstrong_calc += pow(digit, digits_count);
    }
 
