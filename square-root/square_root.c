45a2aa1c249d632c51de191153a1ffd9425fd4a9
coaxial <c@64b.it>
2023-03-27 13:36:39 +0200

    Improve square-root solution

--- square-root/square_root.c
+++ square-root/square_root.c
@@ -1,9 +1,26 @@
 #include "square_root.h"
 #include <math.h>
 #include <assert.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+static bool is_close_enough(int guess, int radicand) {
+   return abs((guess * guess) - radicand) < 0.1;
+}
+
+static int improve(int guess, int radicand) {
+   return ((radicand / guess) + guess) / 2;
+}
+
+static int iterate_sqrt(int guess, int radicand) {
+   if (is_close_enough(guess, radicand)) {
+      return guess;
+   }
+
+   return iterate_sqrt(improve(guess, radicand), radicand);
+}
 
 int square_root(int radicand) {
-   assert(!radicand || radicand > 0);
-
-   return sqrt(radicand);
+   // Start off with 1 as our guess.
+   return iterate_sqrt(1, radicand);
 }
